ef54922be2259306f72b48a15b04878681d89630
Vahe Minasian <vahe_minasian@gmx.com>
2020-01-17 22:21:57 +0100

    removed custom cursor experimental code

--- src/control/MenuController.java
+++ src/control/MenuController.java
@@ -32,9 +32,8 @@ public class MenuController {
 	private static BufferedReader reader;
 	private static FileReader file;
 	private static ArrayList<String> profile = new ArrayList<>();
-//	Toolkit toolkit = Toolkit.getDefaultToolkit();
-//	Cursor hourglassCursor = toolkit.createCustomCursor(toolkit.getImage(getClass().getResource("/pattern.png")), new Point(0,0), "hourGlass");
-	Cursor hourGlassCursor = new Cursor(Cursor.WAIT_CURSOR);
+
+	Cursor waitingCursor = new Cursor(Cursor.WAIT_CURSOR);
 	Cursor normalCursor = new Cursor(Cursor.DEFAULT_CURSOR);
 	private MemoryGame gameModel;
 	private GameView gameView;
@@ -488,8 +487,8 @@ public class MenuController {
 		Card card;
 		int xIndexComp;
 		int yIndexComp;
-//		gameView.setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new ImageIcon(getClass.getResource("/blue-hourglass.gif")).getImage(),new Point(0,0),"custom cursor"));
-		gameView.setCursor(hourGlassCursor);
+
+		gameView.setCursor(waitingCursor);
 		System.out.println("Beginning of StartGame...active player is: " + gameModel.getActivePlayer().getName());
 
 		do {
